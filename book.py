3b9fe4689e7f93e0c083d6fa4ff2256f5d46b5ef
Kiran pokhrel <you@example.com>
2018-08-13 16:31:07 +0545

    test

--- book.py
+++ book.py
@@ -23,3 +23,4 @@ class Book:
 
     def __str__(self):
         return self.name +","+self.ids+","self.name+","self.author+","self.eddition
+    kjsdsjdfnalskdfnksdmlksndlkasndksndclksmdclknsdknsadkocjn iama bhai 
